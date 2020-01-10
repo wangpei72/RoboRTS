626e726d0cdcd4aca1d26fbf496dc77733c320fd
jun0315 <1072505283@qq.com>
2020-01-10 21:38:43 +0800

    Delete cmake-build-debug

--- roborts_camera/cmake-build-debug/mvs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wang_shuai/vwbot_ws/src/roborts_camera/mvs
-# Build directory: /home/wang_shuai/vwbot_ws/src/roborts_camera/cmake-build-debug/mvs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
