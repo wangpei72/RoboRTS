bb8d7b3215fa0ecd1d9ed38ccf37333699797071
Kehan.Xue <kehan.xue@gmail.com>
2020-02-29 23:32:46 +0800

    Stage...

--- roborts_decision/blackboard/my_robot.cpp
+++ roborts_decision/blackboard/my_robot.cpp
@@ -3,3 +3,143 @@
 //
 
 #include "my_robot.h"
+using namespace roborts_decision;
+
+MyRobot::MyRobot(const ros::NodeHandle &nh) {
+
+}
+
+MyRobot::~MyRobot() {
+
+}
+
+RobotId MyRobot::GetId() const {
+  return id_;
+}
+
+void MyRobot::SetId(RobotId id) {
+  id_ = id;
+}
+
+int MyRobot::GetHp() const {
+  return hp_;
+}
+
+void MyRobot::SetHp(int hp) {
+  hp_ = hp;
+}
+
+int MyRobot::GetCurrentHeat() const {
+  return current_heat_;
+}
+
+void MyRobot::SetCurrentHeat(int current_heat) {
+  current_heat_ = current_heat;
+}
+
+int MyRobot::GetRemainingProjectiles() const {
+  return remaining_projectiles_;
+}
+
+void MyRobot::SetRemainingProjectiles(int remaining_projectiles) {
+  remaining_projectiles_ = remaining_projectiles;
+}
+
+const std::vector<ArmorId> &MyRobot::GetArmorsUnderAttack() const {
+  return armors_under_attack_;
+}
+
+void MyRobot::SetArmorsUnderAttack(const std::vector<ArmorId> &armors_under_attack) {
+  armors_under_attack_ = armors_under_attack;
+}
+
+const roborts_msgs::ArmorsDetected &MyRobot::GetArmorsInEyes() const {
+  return armors_in_eyes_;
+}
+
+void MyRobot::SetArmorsInEyes(const roborts_msgs::ArmorsDetected &armors_in_eyes) {
+  armors_in_eyes_ = armors_in_eyes;
+}
+
+const geometry_msgs::PoseStamped &MyRobot::GetChassisMapPose() const {
+  return chassis_map_pose_;
+}
+
+void MyRobot::SetChassisMapPose(const geometry_msgs::PoseStamped &chassis_map_pose) {
+  chassis_map_pose_ = chassis_map_pose;
+}
+
+const geometry_msgs::PoseStamped &MyRobot::GetChassisOdomPose() const {
+  return chassis_odom_pose_;
+}
+
+void MyRobot::SetChassisOdomPose(const geometry_msgs::PoseStamped &chassis_odom_pose) {
+  chassis_odom_pose_ = chassis_odom_pose;
+}
+
+const geometry_msgs::PoseStamped &MyRobot::GetGimbalMapPose() const {
+  return gimbal_map_pose_;
+}
+
+void MyRobot::SetGimbalMapPose(const geometry_msgs::PoseStamped &gimbal_map_pose) {
+  gimbal_map_pose_ = gimbal_map_pose;
+}
+
+const geometry_msgs::PoseStamped &MyRobot::GetGimbalOdomPose() const {
+  return gimbal_odom_pose_;
+}
+
+void MyRobot::SetGimbalOdomPose(const geometry_msgs::PoseStamped &gimbal_odom_pose) {
+  gimbal_odom_pose_ = gimbal_odom_pose;
+}
+
+const geometry_msgs::PoseStamped &MyRobot::GetCurrentGoal() const {
+  return current_goal_;
+}
+
+void MyRobot::SetCurrentGoal(const geometry_msgs::PoseStamped &current_goal) {
+  current_goal_ = current_goal;
+}
+
+RmRobotBehavior MyRobot::GetCurrentBehavior() const {
+  return current_behavior_;
+}
+
+void MyRobot::SetCurrentBehavior(RmRobotBehavior current_behavior) {
+  current_behavior_ = current_behavior;
+}
+
+bool MyRobot::operator==(const MyRobot &rhs) const {
+
+  if (!IsPoseMsgEqual(chassis_map_pose_, rhs.chassis_map_pose_)) {
+    return false;
+  }
+
+  if (!IsPoseMsgEqual(chassis_odom_pose_, rhs.chassis_odom_pose_)) {
+    return false;
+  }
+
+  if (!IsPoseMsgEqual(gimbal_map_pose_, rhs.gimbal_map_pose_)) {
+    return false;
+  }
+
+  if (!IsPoseMsgEqual(gimbal_odom_pose_, rhs.gimbal_odom_pose_)) {
+    return false;
+  }
+
+  if (!IsPoseMsgEqual(current_goal_, rhs.current_goal_)) {
+    return false;
+  }
+
+  return id_ == rhs.id_ &&
+      hp_ == rhs.hp_ &&
+      current_heat_ == rhs.current_heat_ &&
+      remaining_projectiles_ == rhs.remaining_projectiles_ &&
+      armors_under_attack_ == rhs.armors_under_attack_ &&
+      armors_in_eyes_ == rhs.armors_in_eyes_ &&
+      current_behavior_ == rhs.current_behavior_;
+}
+
+bool MyRobot::operator!=(const MyRobot &rhs) const {
+  return !(rhs == *this);
+}
--- roborts_decision/blackboard/my_robot.h
+++ roborts_decision/blackboard/my_robot.h
@@ -18,14 +18,63 @@
 namespace roborts_decision {
 
 class MyRobot {
+ public:
+
+  explicit MyRobot(const ros::NodeHandle &nh = ros::NodeHandle("~"));
+  virtual ~MyRobot();
+
+  RobotId GetId() const;
+  void SetId(RobotId id);
+
+  int GetHp() const;
+  void SetHp(int hp);
+
+  int GetCurrentHeat() const;
+  void SetCurrentHeat(int current_heat);
+
+  int GetRemainingProjectiles() const;
+  void SetRemainingProjectiles(int remaining_projectiles);
+
+  const std::vector<ArmorId> &GetArmorsUnderAttack() const;
+  void SetArmorsUnderAttack(const std::vector<ArmorId> &armors_under_attack);
+
+  const roborts_msgs::ArmorsDetected &GetArmorsInEyes() const;
+  void SetArmorsInEyes(const roborts_msgs::ArmorsDetected &armors_in_eyes);
+
+  const geometry_msgs::PoseStamped &GetChassisMapPose() const;
+  void SetChassisMapPose(const geometry_msgs::PoseStamped &chassis_map_pose);
+
+  const geometry_msgs::PoseStamped &GetChassisOdomPose() const;
+  void SetChassisOdomPose(const geometry_msgs::PoseStamped &chassis_odom_pose);
+
+  const geometry_msgs::PoseStamped &GetGimbalMapPose() const;
+  void SetGimbalMapPose(const geometry_msgs::PoseStamped &gimbal_map_pose);
+
+  const geometry_msgs::PoseStamped &GetGimbalOdomPose() const;
+  void SetGimbalOdomPose(const geometry_msgs::PoseStamped &gimbal_odom_pose);
+
+  const geometry_msgs::PoseStamped &GetCurrentGoal() const;
+  void SetCurrentGoal(const geometry_msgs::PoseStamped &current_goal);
+
+  RmRobotBehavior GetCurrentBehavior() const;
+  void SetCurrentBehavior(RmRobotBehavior current_behavior);
+
+  bool operator==(const MyRobot &rhs) const;
+  bool operator!=(const MyRobot &rhs) const;
 
  private:
 
+  ros::NodeHandle nh_;
+  ros::Subscriber armors_under_attack_sub_;
+  ros::Subscriber armors_in_eyes_sub_;
+
   std::shared_ptr<tf::TransformListener> tf_ptr_;
 
-  int id_;
+  RobotId id_;
   int hp_;
+  int current_heat_;
   int remaining_projectiles_;
+
   std::vector<ArmorId> armors_under_attack_;
   roborts_msgs::ArmorsDetected armors_in_eyes_;
 
@@ -35,8 +84,8 @@ class MyRobot {
   geometry_msgs::PoseStamped gimbal_map_pose_;
   geometry_msgs::PoseStamped gimbal_odom_pose_;
 
-  RmRobotBehavior current_behavior_;
   geometry_msgs::PoseStamped current_goal_;
+  RmRobotBehavior current_behavior_;
 
 };
 
